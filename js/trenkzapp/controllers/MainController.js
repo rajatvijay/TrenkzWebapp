6b6063a27c229c0e23425f11507f095295e65e95
Rajat Vijay <rajatvijay5@gmail.com>
2016-02-09 00:34:16 +0530

    Added event handlers for the category navigation

--- js/trenkzapp/controllers/MainController.js
+++ js/trenkzapp/controllers/MainController.js
@@ -1,14 +1,65 @@
 app.controller('MainController', [
-	'$scope', function($scope) {
+	'$scope', function ($scope) {
 		$scope.categories = [
 			{
-				name: "Oriental",
-				price: 2000
+				name: "dance",
+				id: 1
 			},
 			{
-				name: "Sulekha",
-				price: 3000
+				name: "singing",
+				id: 2
 			}
-		];
+		]
+		$scope.data = [
+			{
+				id: 1,
+				category: "dance",
+				classes: [
+					{
+						name: "Steps and Rhythm",
+						address: "B-38 Triveni Nagar",
+						price: 1000,
+						contact_no: "9602833112",
+						image: ""
+					},
+					{
+						name: "Steps and Rhythm",
+						address: "B-38 Triveni Nagar",
+						price: 1000,
+						contact_no: "9602833112",
+						image: ""
+					}
+				]
+			},
+			{
+				id: 2,
+				category: "singing",
+				classes: [
+					{
+						name: "",
+						address: "B-38 Triveni Nagar",
+						price: 1000,
+						contact_no: "9602833112",
+						image: ""
+					},
+					{
+						name: "Steps and Rhythm",
+						address: "B-38 Triveni Nagar",
+						price: 1000,
+						contact_no: "9602833112",
+						image: ""
+					}
+				]
+			}
+		]
+		$scope.cat = $scope.data[0];
+		$scope.doClick = function (id, event) {
+			for (var index = 0; index < $scope.data.length; index++) {
+				if (id == $scope.data[index].id) {
+					$scope.cat = $scope.data[index];
+				}
+			}
+		};
+
 	}
 ]);
